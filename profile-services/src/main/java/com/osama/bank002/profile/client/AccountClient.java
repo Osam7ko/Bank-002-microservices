5723974374e69e62bc637c609c9081baeb2f086d
OS-TB\osam7 <Osam7ko>
2025-08-09 17:23:35 +0300

    adding account calls

--- profile-services/src/main/java/com/osama/bank002/profile/client/AccountClient.java
+++ profile-services/src/main/java/com/osama/bank002/profile/client/AccountClient.java
@@ -3,6 +3,7 @@ package com.osama.bank002.profile.client;
 import com.osama.bank002.profile.client.dto.BankResponse;
 import com.osama.bank002.profile.client.dto.OpenAccountRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "ACCOUNT-SERVICE")
@@ -11,4 +12,6 @@ public interface AccountClient {
     @PostMapping("/api/accounts")
     BankResponse open(OpenAccountRequest req);
 
+    @GetMapping("api/accounts/owner/{profileId}/count")
+    int countOpenAccounts(String uid);
 }
